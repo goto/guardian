25daa4b785f0c0950a7e7f3684ceff2fd5715e49
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2023-03-24 11:31:29 +0700

    refactor(bigquery): fetch tables concurrently

--- plugins/providers/bigquery/provider.go
+++ plugins/providers/bigquery/provider.go
@@ -16,6 +16,7 @@ import (
 	"github.com/goto/salt/log"
 	"github.com/mitchellh/mapstructure"
 	"github.com/patrickmn/go-cache"
+	"golang.org/x/sync/errgroup"
 )
 
 var (
@@ -120,32 +121,40 @@ func (p *Provider) GetResources(pc *domain.ProviderConfig) ([]*domain.Resource, 
 	resourceTypes := pc.GetResourceTypes()
 
 	resources := []*domain.Resource{}
-	ctx := context.Background()
+	eg, ctx := errgroup.WithContext(context.TODO())
+
 	datasets, err := client.GetDatasets(ctx)
 	if err != nil {
 		return nil, err
 	}
 	for _, d := range datasets {
-		dataset := d.ToDomain()
-		dataset.ProviderType = pc.Type
-		dataset.ProviderURN = pc.URN
+		d := d
+		eg.Go(func() error {
+			dataset := d.ToDomain()
+			dataset.ProviderType = pc.Type
+			dataset.ProviderURN = pc.URN
 
-		if containsString(resourceTypes, ResourceTypeDataset) {
-			resources = append(resources, dataset)
-		}
+			if containsString(resourceTypes, ResourceTypeDataset) {
+				resources = append(resources, dataset)
+			}
 
-		if containsString(resourceTypes, ResourceTypeTable) {
-			tables, err := client.GetTables(ctx, dataset.Name)
-			if err != nil {
-				return nil, err
+			if containsString(resourceTypes, ResourceTypeTable) {
+				tables, err := client.GetTables(ctx, dataset.Name)
+				if err != nil {
+					return fmt.Errorf("fetching tables for dataset %q: %w", dataset.URN, err)
+				}
+				for _, t := range tables {
+					table := t.ToDomain()
+					table.ProviderType = pc.Type
+					table.ProviderURN = pc.URN
+					dataset.Children = append(dataset.Children, table)
+				}
 			}
-			for _, t := range tables {
-				table := t.ToDomain()
-				table.ProviderType = pc.Type
-				table.ProviderURN = pc.URN
-				dataset.Children = append(dataset.Children, table)
-			}
-		}
+			return nil
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return nil, err
 	}
 
 	return resources, nil
