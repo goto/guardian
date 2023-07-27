1ef386053cdfd3dac22026ed74f6f16a653a3fbf
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2023-07-27 09:56:49 +0700

    fix: read raw config

--- jobs/grant_dormancy_check.go
+++ jobs/grant_dormancy_check.go
@@ -9,16 +9,29 @@ import (
 )
 
 type GrantDormancyCheckConfig struct {
-	DryRun         bool          `mapstructure:"dry_run"`
-	StartDate      time.Time     `mapstructure:"start_date"`
-	EndDate        time.Time     `mapstructure:"end_date"`
-	RetainGrantFor time.Duration `mapstructure:"retain_grant_for"`
+	DryRun         bool   `mapstructure:"dry_run"`
+	StartDate      string `mapstructure:"start_date"`
+	EndDate        string `mapstructure:"end_date"`
+	RetainGrantFor string `mapstructure:"retain_grant_for"`
 }
 
 func (h *handler) GrantDormancyCheck(ctx context.Context, c Config) error {
 	var cfg GrantDormancyCheckConfig
 	if err := c.Decode(&cfg); err != nil {
 		return fmt.Errorf("invalid config for %s job: %w", TypeRevokeGrantsByUserCriteria, err)
+	}
+
+	startDate, err := time.Parse(time.RFC3339, cfg.StartDate)
+	if err != nil {
+		return fmt.Errorf("invalid start date: %w", err)
+	}
+	endDate, err := time.Parse(time.RFC3339, cfg.EndDate)
+	if err != nil {
+		return fmt.Errorf("invalid end date: %w", err)
+	}
+	retainGrantFor, err := time.ParseDuration(cfg.RetainGrantFor)
+	if err != nil {
+		return fmt.Errorf("invalid retain grant for duration: %w", err)
 	}
 
 	providers, err := h.providerService.Find(ctx)
@@ -30,9 +43,9 @@ func (h *handler) GrantDormancyCheck(ctx context.Context, c Config) error {
 		h.logger.Info(fmt.Sprintf("checking dormancy for grants under provider: %q", p.URN))
 		if err := h.grantService.DormancyCheck(ctx, domain.DormancyCheckCriteria{
 			ProviderID:     p.ID,
-			TimestampeGte:  cfg.StartDate,
-			TimestampeLte:  cfg.EndDate,
-			RetainDuration: cfg.RetainGrantFor,
+			TimestampeGte:  startDate,
+			TimestampeLte:  endDate,
+			RetainDuration: retainGrantFor,
 			DryRun:         cfg.DryRun,
 		}); err != nil {
 			h.logger.Error(fmt.Sprintf("failed to check dormancy for provider %q", p.URN), "error", err)
