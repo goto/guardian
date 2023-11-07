85979dc5a6c112f06c3ad84691e58db8d3dd1e99
sushmith <bsushmith@live.com>
2023-11-07 14:57:07 +0530

    test: update test cases

--- pkg/log/logger_test.go
+++ pkg/log/logger_test.go
@@ -13,31 +13,31 @@ func TestLogger(t *testing.T) {
 
 	t.Run("empty context", func(t *testing.T) {
 		t.Run("Debug", func(t *testing.T) {
-			saltLogger.EXPECT().Debug("this is a test debug message", []interface{}{"keys", "test-value"}).Once()
+			saltLogger.EXPECT().Debug("this is a test debug message", []interface{}{"keys", "test-value"}...).Once()
 			l.Debug(nil, "this is a test debug message", "keys", "test-value")
 			saltLogger.AssertExpectations(t)
 		})
 
 		t.Run("Info", func(t *testing.T) {
-			saltLogger.EXPECT().Info("this is a test info message", []interface{}{"keys", "test-value"}).Once()
+			saltLogger.EXPECT().Info("this is a test info message", []interface{}{"keys", "test-value"}...).Once()
 			l.Info(nil, "this is a test info message", "keys", "test-value")
 			saltLogger.AssertExpectations(t)
 		})
 
 		t.Run("Warn", func(t *testing.T) {
-			saltLogger.EXPECT().Warn("this is a test warn message", []interface{}{"keys", "test-value"}).Once()
+			saltLogger.EXPECT().Warn("this is a test warn message", []interface{}{"keys", "test-value"}...).Once()
 			l.Warn(nil, "this is a test warn message", "keys", "test-value")
 			saltLogger.AssertExpectations(t)
 		})
 
 		t.Run("Error", func(t *testing.T) {
-			saltLogger.EXPECT().Error("this is a test error message", []interface{}{"keys", "test-value"}).Once()
+			saltLogger.EXPECT().Error("this is a test error message", []interface{}{"keys", "test-value"}...).Once()
 			l.Error(nil, "this is a test error message", "keys", "test-value")
 			saltLogger.AssertExpectations(t)
 		})
 
 		t.Run("Fatal", func(t *testing.T) {
-			saltLogger.EXPECT().Fatal("this is a test fatal message", []interface{}{"keys", "test-value"}).Once()
+			saltLogger.EXPECT().Fatal("this is a test fatal message", []interface{}{"keys", "test-value"}...).Once()
 			l.Fatal(nil, "this is a test fatal message", "keys", "test-value")
 			saltLogger.AssertExpectations(t)
 		})
@@ -46,31 +46,31 @@ func TestLogger(t *testing.T) {
 	t.Run("context with keys", func(t *testing.T) {
 		ctx := context.WithValue(context.Background(), "ctx-key-1", "ctx-value-1")
 		t.Run("Debug", func(t *testing.T) {
-			saltLogger.EXPECT().Debug("this is a test debug message", []interface{}{"key1", "test-value1", "ctx-key-1", "ctx-value-1"}).Once()
+			saltLogger.EXPECT().Debug("this is a test debug message", []interface{}{"key1", "test-value1", "ctx-key-1", "ctx-value-1"}...).Once()
 			l.Debug(ctx, "this is a test debug message", "key1", "test-value1")
 			saltLogger.AssertExpectations(t)
 		})
 
 		t.Run("Info", func(t *testing.T) {
-			saltLogger.EXPECT().Info("this is a test info message", []interface{}{"key1", "test-value1", "ctx-key-1", "ctx-value-1"}).Once()
+			saltLogger.EXPECT().Info("this is a test info message", []interface{}{"key1", "test-value1", "ctx-key-1", "ctx-value-1"}...).Once()
 			l.Info(ctx, "this is a test info message", "key1", "test-value1")
 			saltLogger.AssertExpectations(t)
 		})
 
 		t.Run("Warn", func(t *testing.T) {
-			saltLogger.EXPECT().Warn("this is a test warn message", []interface{}{"key1", "test-value1", "ctx-key-1", "ctx-value-1"}).Once()
+			saltLogger.EXPECT().Warn("this is a test warn message", []interface{}{"key1", "test-value1", "ctx-key-1", "ctx-value-1"}...).Once()
 			l.Warn(ctx, "this is a test warn message", "key1", "test-value1")
 			saltLogger.AssertExpectations(t)
 		})
 
 		t.Run("Error", func(t *testing.T) {
-			saltLogger.EXPECT().Error("this is a test error message", []interface{}{"key1", "test-value1", "ctx-key-1", "ctx-value-1"}).Once()
+			saltLogger.EXPECT().Error("this is a test error message", []interface{}{"key1", "test-value1", "ctx-key-1", "ctx-value-1"}...).Once()
 			l.Error(ctx, "this is a test error message", "key1", "test-value1")
 			saltLogger.AssertExpectations(t)
 		})
 
 		t.Run("Fatal", func(t *testing.T) {
-			saltLogger.EXPECT().Fatal("this is a test fatal message", []interface{}{"key1", "test-value1", "ctx-key-1", "ctx-value-1"}).Once()
+			saltLogger.EXPECT().Fatal("this is a test fatal message", []interface{}{"key1", "test-value1", "ctx-key-1", "ctx-value-1"}...).Once()
 			l.Fatal(ctx, "this is a test fatal message", "key1", "test-value1")
 			saltLogger.AssertExpectations(t)
 		})
