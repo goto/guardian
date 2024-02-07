a6e9459cf8c9bcdb6ba1a4d674869cbe9d360bcd
Abhishek Verma <abhishek.verma@gojek.com>
2024-02-07 13:57:26 +0530

    chore: remove unused error

--- core/grant/errors.go
+++ core/grant/errors.go
@@ -3,9 +3,8 @@ package grant
 import "errors"
 
 var (
-	ErrEmptyIDParam         = errors.New("grant id can't be empty")
-	ErrGrantNotFound        = errors.New("grant not found")
-	ErrEmptyImportedGrants  = errors.New("imported grants not found")
-	ErrEmptyOwner           = errors.New("owner can't be empty")
-	ErrDuplicateActiveGrant = errors.New("grant already exists")
+	ErrEmptyIDParam        = errors.New("grant id can't be empty")
+	ErrGrantNotFound       = errors.New("grant not found")
+	ErrEmptyImportedGrants = errors.New("imported grants not found")
+	ErrEmptyOwner          = errors.New("owner can't be empty")
 )
