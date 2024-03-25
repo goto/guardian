55363b4db2c53dd665d882e687b2148d92ee5668
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2024-03-26 00:55:42 +0700

    fix: squash mapstructure embedded struct

--- core/policy/metadata_source.go
+++ core/policy/metadata_source.go
@@ -3,6 +3,6 @@ package policy
 import "github.com/goto/guardian/pkg/http"
 
 type AppealMetadataSourceConfigHTTP struct {
-	http.HTTPClientConfig
-	AllowFailed bool `mapstructure:"allow_failed,omitempty" json:"allow_failed,omitempty" yaml:"allow_failed,omitempty"`
+	http.HTTPClientConfig `mapstructure:",squash"`
+	AllowFailed           bool `mapstructure:"allow_failed,omitempty" json:"allow_failed,omitempty" yaml:"allow_failed,omitempty"`
 }
