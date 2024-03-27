f67976c544ae1b47dc7f8cb5247499fe39264346
Muhammad Idil Haq Amir <idil.amir@gotocompany.com>
2024-03-27 15:22:08 +0700

    chore: update round trip flow
    Co-authored-by: StewartJingga <stewart_jingga@yahoo.com>

--- pkg/http/retry.go
+++ pkg/http/retry.go
@@ -14,30 +14,27 @@ type RetryableTransport struct {
 }
 
 func (t *RetryableTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	// Clone the request body
 	var bodyBytes []byte
-	if req.Body != nil {
-		bodyBytes, _ = io.ReadAll(req.Body)
-		req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+	bodyBytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading body: %w", err)
 	}
-	// Send the request
-	resp, err := t.Transport.RoundTrip(req)
-	// Retry logic
-	retries := 0
+
+	var resp *http.Response
+	retries := -1
 	for shouldRetry(err, resp) && retries < t.RetryCount {
-		// Wait for the specified backoff period
-		time.Sleep(backoff(retries))
-		// We're going to retry, consume any response to reuse the connection.
-		drainBody(resp)
-		// Clone the request body again
-		if req.Body != nil {
-			req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+		if retries > -1 {
+			time.Sleep(backoff(retries))
+			// consume any response to reuse the connection.
+			drainBody(resp)
 		}
-		// Retry the request
+
+		req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 		resp, err = t.Transport.RoundTrip(req)
+
 		retries++
 	}
-	// Return the response
+
 	return resp, err
 }
 
