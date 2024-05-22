e4a8c290446872f9002fc8e57911188c04895f3c
Ayushi Sharma <ayushi.sharma@gojek.com>
2024-05-22 06:56:26 +0530

    feat(edit-appeal): added loggers

--- core/appeal/service.go
+++ core/appeal/service.go
@@ -693,6 +693,7 @@ func (s *Service) Patch(ctx context.Context, appeal *domain.Appeal, opts ...Crea
 		}
 	}
 
+	s.logger.Error(ctx, "modified appeal: %w", appeal)
 	if err := s.repo.UpdateByID(ctx, appeal); err != nil {
 		return fmt.Errorf("error saving appeal to db: %w", err)
 	}
@@ -702,6 +703,7 @@ func (s *Service) Patch(ctx context.Context, appeal *domain.Appeal, opts ...Crea
 		return err
 	}
 
+	s.logger.Error(ctx, "audit log: %w", log)
 	if err := s.auditLogger.Log(ctx, AuditKeyUpdateAppeal, log); err != nil {
 		s.logger.Error(ctx, "failed to record audit log", "error", err)
 	}
