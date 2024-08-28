cfadb8f9f976e7d7dba3f13ac3c2ce245f299f8f
Lifosmin Simon <lifosmin.simon@gojek.com>
2024-08-28 15:05:22 +0700

    fix: load telemetry for cli

--- config/telemetry.go
+++ config/telemetry.go
@@ -5,13 +5,13 @@ import "time"
 type TelemetryConfig struct {
 	Enabled        bool              `mapstructure:"enabled" default:"false"`
 	ServiceName    string            `mapstructure:"service_name" default:"guardian"`
-	ServiceVersion string            `mapstructure:"service_version" default:"otlp.nr-data.net:443"`
+	ServiceVersion string            `mapstructure:"service_version"`
 	Labels         map[string]string `mapstructure:"labels"`
 	Exporter       string            `mapstructure:"exporter" default:"stdout"`
 	OTLP           struct {
 		Headers  map[string]string `mapstructure:"headers"`
 		Endpoint string            `mapstructure:"endpoint" default:"otlp.nr-data.net:443"`
 	} `mapstructure:"otlp"`
-	SamplingFraction int           `mapstructure:"sampling_fraction" default:"otlp.nr-data.net:443"`
-	MetricInterval   time.Duration `mapstructure:"metric_interval" default:"otlp.nr-data.net:443"`
+	SamplingFraction int           `mapstructure:"sampling_fraction"`
+	MetricInterval   time.Duration `mapstructure:"metric_interval"`
 }
