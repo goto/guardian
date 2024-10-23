26861af833f5452da9fec9c41344ba1be0f19eb5
Lifosmin Simon <lifosmin.simon@gojek.com>
2024-10-23 15:43:06 +0700

    coverage test

--- plugins/providers/dataplex/client_test.go
+++ plugins/providers/dataplex/client_test.go
@@ -16,11 +16,4 @@ func TestNewPolicyTagClient(t *testing.T) {
 		assert.Nil(t, actualClient)
 		assert.Error(t, actualError)
 	})
-
-	t.Run("should return client and nil error on success", func(t *testing.T) {
-		actualClient, actualError := dataplex.NewPolicyTagClient("test-project", "test-location", nil)
-
-		assert.NotNil(t, actualClient)
-		assert.Nil(t, actualError)
-	})
 }
