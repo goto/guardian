14f16dfef929e63081904d8d407aef7a1843cd6c
Lifosmin Simon <lifosmin.simon@gojek.com>
2024-10-28 17:58:50 +0700

    resolve comments

--- pkg/opentelemetry/otelhttpclient/client.go
+++ pkg/opentelemetry/otelhttpclient/client.go
@@ -6,18 +6,10 @@ import (
 
 func New(name string, client *http.Client) *http.Client {
 	if client == nil {
-		return NewClient(name)
+		return &http.Client{
+			Transport: NewHTTPTransport(nil, name),
+		}
 	}
-	return NewFromClient(client, name)
-}
-
-func NewClient(name string) *http.Client {
-	return &http.Client{
-		Transport: NewHTTPTransport(nil, name),
-	}
-}
-
-func NewFromClient(httpClient *http.Client, name string) *http.Client {
-	httpClient.Transport = NewHTTPTransport(httpClient.Transport, name)
-	return httpClient
+	client.Transport = NewHTTPTransport(client.Transport, name)
+	return client
 }
