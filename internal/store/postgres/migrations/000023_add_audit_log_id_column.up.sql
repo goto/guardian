2131ec830e0d7f6cc5dc375157ce8386e23671cb
Lifosmin Simon <lifosmin.simon@gojek.com>
2025-02-07 15:21:28 +0700

    remove redudant function

--- internal/store/postgres/migrations/000023_add_audit_log_id_column.up.sql
+++ internal/store/postgres/migrations/000023_add_audit_log_id_column.up.sql
@@ -3,10 +3,6 @@ BEGIN;
 ALTER TABLE "audit_logs"
 ADD COLUMN IF NOT EXISTS "id" UUID DEFAULT uuid_generate_v4();
 
-UPDATE "audit_logs"
-SET "id" = uuid_generate_v4()
-WHERE "id" IS NULL;
-
 DO $$ 
 BEGIN
   IF NOT EXISTS (
