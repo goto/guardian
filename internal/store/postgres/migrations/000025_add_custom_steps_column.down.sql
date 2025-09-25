32e3e084dc5489794f09bcde332bae37075d7315
Anjali Aggarwal <anjali.aggarwal@gojek.com>
2025-09-26 01:31:29 +0530

    chore: update migration file numbers

--- internal/store/postgres/migrations/000025_add_custom_steps_column.down.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-ALTER TABLE
-    "policies"
-DROP COLUMN IF EXISTS "custom_steps";
-
-COMMIT;
--- internal/store/postgres/migrations/000025_add_custom_steps_column.up.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-ALTER TABLE
-    "policies"
-    ADD COLUMN IF NOT EXISTS "custom_steps" jsonb;
-
-COMMIT;
