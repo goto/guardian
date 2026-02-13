6ca9daa84f5b27021cfd5c37a7cbd7f914569050
bearaujus <haryo.assyafah@gopay.co.id>
2026-02-13 16:26:24 +0700

    dev

--- core/appeal/service.go
+++ core/appeal/service.go
@@ -1965,9 +1965,8 @@ func (s *Service) populateAppealMetadata(ctx context.Context, a *domain.Appeal, 
 					return err
 				}
 
-				headers := make(map[string]string)
 				for k, v := range cfg.Headers {
-					if headers[k], err = evaluateExpressionWithAppeal(a, v); err != nil {
+					if cfg.Headers[k], err = evaluateExpressionWithAppeal(a, v); err != nil {
 						return err
 					}
 				}
